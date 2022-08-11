1a5055f80b70aae62186f6cbf3c80c23bd27f8bd
manhtuan07 <manhtuan7197@gmail.com>
2022-08-11 13:28:37 +0700

    Initial commit

---
+++ src/test/java/com/example/democicdjenkins/DemoCicdJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democicdjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
